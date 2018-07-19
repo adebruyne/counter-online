56fedc63cc83b1886757acc097b848cc3b49e161
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-19 15:14:15 -0400

    adding lists

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,24 +3,25 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    imageUrl: "https://picsum.photos/200",
+    tags: ["tag1", "tag2", "tag3"]
   };
-
-  //   styles = {
-  //     fontSize: 20,
-  //     fontWeight: "bold"
-  //   };
 
   render() {
     return (
-      <React.Fragment>
+      <div>
         <img src={this.state.imageUrl} alt="" />
-        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
-          {this.formatCount()}
-        </span>
+        <span className={this.getBadgeClass()}>{this.formatCount()}</span>
         <button className="btn btn-secondary btn-sm">Increment</button>
-      </React.Fragment>
+        <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>
+      </div>
     );
+  }
+
+  getBadgeClass() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
   }
 
   formatCount() {
