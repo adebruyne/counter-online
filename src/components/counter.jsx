42f4493afdad470e6ceea7fadebd8aefe5bb40d1
Aylin DeBruyne <adebruyne88@gmail.com>
2018-07-19 14:58:39 -0400

    playing with styles

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,25 +1,32 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
-    state = {
-        count: 0
-    };
+  state = {
+    count: 0,
+    imageUrl: "https://picsum.photos/200"
+  };
+
+  //   styles = {
+  //     fontSize: 20,
+  //     fontWeight: "bold"
+  //   };
 
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <img src={this.state.imageUrl} alt="" />
+        <span style={{ fontSize: 30 }} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </React.Fragment>
     );
   }
 
   formatCount() {
-      const {count} = this.state;
-      return count === 0 ? <h1>Zero</h1> : count; 
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
-
-
 }
 
 export default Counter;
